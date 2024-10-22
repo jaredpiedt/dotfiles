c44f75cf2b900508ef70e3b638d1aeaddf662e08
Jared Piedt <jared@piedt.com>
2024-10-22 15:58:34 -0400

    update git config

--- nix/home/git.nix
+++ nix/home/git.nix
@@ -4,27 +4,51 @@
   ...
 }: {
   enable = true;
-  aliases
-  lfs.enable = true;
   userName = "Jared Piedt";
   userEmail = "jared@piedt.com";
   signing.key = null;
   signing.signByDefault = true;
-
   extraConfig = {
+    color = {
+      "branch" = {
+        current = "yellow reverse";
+        local = "yellow";
+        remote = "green";
+      };
+      "diff" = {
+        meta = "yellow bold";
+        frag = "magenta bold";
+        old = "red";
+        new = "green";
+      };
+      "status" = {
+        added = "yellow";
+        changed = "green";
+        untracked = "cyan";
+      };
+    };
     merge = {
       log = true;
     };
     url = {
+      # Multiple insteadOf configurations for GitHub
       "git@github.com:" = {
         insteadOf = "https://github.com/";
-        insteadOf = "github:";
-	      insteadOf = "git://github.com/"
       };
+      "github:" = {
+        insteadOf = "https://github.com/";
+      };
+      "git://github.com/" = {
+        insteadOf = "https://github.com/";
+      };
+      
+      # Multiple pushInsteadOf for gist
       "git@gist.github.com:" = {
         insteadOf = "gst:";
         pushInsteadOf = "gist:";
-        pushInsteadOf = "git://gist.github.com/";
+      };
+      "git://gist.github.com/" = {
+        pushInsteadOf = "gist:";
       };
     };
   };
